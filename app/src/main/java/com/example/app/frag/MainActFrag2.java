b1e134b056d69ba72b73fe0b2c79ceb4ddcc7dc8
Tiago <ggtiago0santos@gmail.com>
2023-05-10 08:46:13 +0100

    a

--- app/src/main/java/com/example/app/frag/MainActFrag2.java
+++ app/src/main/java/com/example/app/frag/MainActFrag2.java
@@ -1,39 +1,39 @@
-package com.example.app.frag;
+package com.example.applicationblow.fragment;
 
 import android.app.Activity;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.app.Fragment;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.app.R;
-
+import com.example.namespace.R;
 
 public class MainActFrag2 extends Activity {
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    Button btn;
+    private FragmentTransaction ft;
+
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mainactfrag2);
-
-        /*
-        Classe FragmentManager é responsável por gerir os fragmentos. add, remove, replace, etc.
-         */
+        btn = (Button) findViewById(R.id.botao1);
+        btn.setOnClickListener((View.OnClickListener) this);
         FragmentManager fm = getFragmentManager();
-        FragmentTransaction ft = fm.beginTransaction();
-        //Exemplo 1 sem args
-        /*
-        ft.add(R.id.fragment_content, new MainFrag2());
+        ft = fm.beginTransaction();
+        /*ft.add(R.id.fragment_content, new MainFrag2());
+        //commit->que valida as transaction
         ft.commit();*/
-
-        //Exemplo 2 com args
         MainFrag2 frag = new MainFrag2();
         Bundle b = new Bundle();
-        String link = "https://www.google.com";
-        b.putString("Link", link);
+        String link = "http://epcc.pt";
+        b.putString("link", link);
         frag.setArguments(b);
-        ft.add(R.id.fragment_content, frag);
+        ft.add(R.id.fragment_content,frag);
+    }
+    public void onClick(View view){
         ft.commit();
-
-
     }
 
 
--- app/src/main/java/com/example/app/frag/MainFrag2.java
+++ app/src/main/java/com/example/app/frag/MainFrag2.java
@@ -1,38 +1,27 @@
-package com.example.app.frag;
-
+package com.example.applicationblow.fragment;
+import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.widget.Toast;
-
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
 
 import com.example.app.R;
 
 public class MainFrag2 extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        inflater.inflate(R.layout.frag_main_2, container, false);
-        return super.onCreateView(inflater, container, savedInstanceState);
+        return inflater.inflate(R.layout.frag_main_2, container, false);
     }
-
-    // Act que vincula o fragmento
-
-
     @Override
-    public void onActivityCreated(Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
+    public void onActivityCreated(Bundle savedInstaceState){
+        super.onActivityCreated(savedInstaceState);
         String link = null;
         Bundle b = getArguments();
-        if (b != null) {
-            link = b.getString("Link");
-            Toast.makeText(getActivity(), link, Toast.LENGTH_SHORT).show();
-        }
+        if(b != null)
+            link = b.getString(link);
+        Toast.makeText(getActivity() ,link, Toast.LENGTH_LONG).show();
     }
-
-
-
-
 }
